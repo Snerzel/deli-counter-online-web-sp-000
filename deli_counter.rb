5bf718e289ed783ee2a2c240ed004593b1a2277e
Cherissa Woudenberg <snerzel@comcast.net>
2021-03-28 19:19:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,13 @@
 katz_deli = []
 # Write your code here.
 def line(katz_deli)
-if katz_deli == 0
-  puts "The line is currently empty."
-else katz_deli
+  phrase = "The line is currently: "
+  if katz_deli.length > 0 
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index + 1}. #{name} "
+    end
+    puts phrase
+  else
+    puts "The line is currently empty."
   end
 end
