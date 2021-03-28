9075e1b7f85bb2d69fb693f0db3a869d0e1dfe2b
Cherissa Woudenberg <snerzel@comcast.net>
2021-03-28 19:19:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 # Write your code here.
 def line(katz_deli)
   phrase = "The line is currently: "
-  if katz_deli.length > 0 
+  if katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
       phrase += "#{index + 1}. #{name} "
     end
